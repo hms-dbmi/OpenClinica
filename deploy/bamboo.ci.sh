328618e1d792d38788142936d59e6114251b0b3d
David Bernick <dbernick@gmail.com>
2014-09-16 13:50:05 -0400

    added deploy

---
+++ deploy/bamboo.ci.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+export PATH=/sbin:/bin:/usr/sbin:/usr/bin
+
+install() {
+ apt-get update
+ apt-get install -y libpq-dev
+ apt-get install -y python-dev
+ apt-get install -y libxml2-dev
+ apt-get install -y libxslt1-dev
+ apt-get install -y python-pip
+ apt-get install -y python-setuptools
+ pip install --upgrade awscli
+}
+
+install >/tmp/startup.log 2>&1
+
+AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3 cp ${bamboo.build.working.directory}/$ORIGARTIFACT s3://cbmi_artifacts/$KEYNAME/$DEVENV/${bamboo.buildNumber}/$ARTIFACT
+AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3 cp ${bamboo.build.working.directory}/$ORIGARTIFACT s3://cbmi_artifacts/$KEYNAME/$DEVENV/latest/$ARTIFACT

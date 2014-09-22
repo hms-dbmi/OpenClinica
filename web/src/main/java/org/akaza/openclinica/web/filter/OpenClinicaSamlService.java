9ad9077e2efaeefddc1da55daf053d17484e8f13
David Bernick <dbernick@gmail.com>
2014-09-22 10:40:38 -0400

    alter security

---
+++ web/src/main/java/org/akaza/openclinica/web/filter/OpenClinicaSamlService.java
@@ -0,0 +1,34 @@
+package org.akaza.openclinica.web.filter;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.GrantedAuthorityImpl;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.saml.SAMLCredential;
+import org.springframework.security.saml.userdetails.SAMLUserDetailsService;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class OpenClinicaSamlService implements SAMLUserDetailsService
+{
+
+       @Override
+       public Object loadUserBySAML(SAMLCredential credential)
+                     throws UsernameNotFoundException
+       {
+              String email = credential.getNameID().getValue();
+
+              GrantedAuthority[] authorities = new GrantedAuthority[] { new GrantedAuthorityImpl("HOLDER") };
+              List<GrantedAuthority> authorities_l = Arrays.<GrantedAuthority>asList(authorities);
+              
+              UserDetails userDetails = new User(
+                           email, "password", true, true, true, true, authorities_l);
+ 
+              return userDetails;
+       }
+}
